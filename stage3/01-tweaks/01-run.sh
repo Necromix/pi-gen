ac239a047404501aa975a1c0a929fac4dc732538
Viktor Verebelyi <vviktor2@gmail.com>
2018-02-27 02:45:03 +0000

    Add more extensive bluetooth rules and add all the new dependencies

--- stage3/01-tweaks/01-run.sh
+++ stage3/01-tweaks/01-run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 
-
+log "Getting udev rules"
 wget -P ${ROOTFS_DIR}/etc/udev/rules.d https://raw.githubusercontent.com/snowdream/51-android/master/51-android.rules
 
+log "copying environment file"
 install -m 644 files/environment ${ROOTFS_DIR}/etc
 
-on_chroot << \EOF
+log "Setting bluetooth permissions"
+on_chroot << EOF
 adduser pi bluetooth
 adduser pulse bluetooth
 EOF
