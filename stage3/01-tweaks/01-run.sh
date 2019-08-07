508baec04ff5cffb7a337dd67f9d8911061da2b1
Viktor Verebelyi <vviktor2@gmail.com>
2019-08-07 17:53:03 +0000

    [STAGE3] Add Pulse Audio system service

--- stage3/01-tweaks/01-run.sh
+++ stage3/01-tweaks/01-run.sh
@@ -3,35 +3,30 @@
 log "Getting udev rules"
 wget -P ${ROOTFS_DIR}/etc/udev/rules.d https://raw.githubusercontent.com/snowdream/51-android/master/51-android.rules
 
-log "copying environment file"
+log "Copy environment file"
 install -m 644 files/environment ${ROOTFS_DIR}/etc
+log "Copy headunit service file"
 install -m 644 files/headunit.service ${ROOTFS_DIR}/lib/systemd/system
-install -m 644 files/splashscreen.service ${ROOTFS_DIR}/lib/systemd/system
-install -m 644 files/boot-logo.bmp ${ROOTFS_DIR}/opt
-
-on_chroot << EOF
-systemctl daemon-reload
-systemctl enable headunit.service
-systemctl enable splashscreen.service
-EOF
-
-log "Setting bluetooth permissions"
-on_chroot << EOF
-adduser pi bluetooth
-adduser pulse bluetooth
-EOF
-
-log "Edit /boot/config.txt and /boot/cmdline.txt"
-on_chroot << EOF
-echo "gpu_mem=256" >> /boot/config.txt
-echo "dtoverlay=vc4-fkms-v3d" >> /boot/config.txt
-echo "lcd_rotate=2" >> /boot/config.txt
-echo "dtoverlay=pi3-disable-bt" >> /boot/config.txt
-echo "disable_splash=1" >> /boot/config.txt
-sed -i ' 1 s/.*/& splash logo.nologo vt.global_cursor_default=0/' /boot/cmdline.txt
-EOF
+log "Copy pulseaudio config file"
+install -m 644 files/default.pa ${ROOTFS_DIR}/home/pi/.config/pulse
 
 log "Run raspberry pi firmware update "
 on_chroot << EOF
-SKIP_WARNING=1 rpi-update
+SKIP_WARNING=1 SKIP_BACKUP=1 rpi-update
+EOF
+
+log "Enable services"
+on_chroot << EOF
+systemctl daemon-reload
+systemctl enable headunit.service
+
+systemctl enable pulseaudio.socket --user
+systemctl enable pulseaudio.service --user
+systemctl enable dbus.service --user
+EOF
+
+log "Set bluetooth permissions"
+on_chroot << EOF
+adduser pi bluetooth
+adduser pulse bluetooth
 EOF
