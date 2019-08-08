389102a583d9b30fd93e2692fb3c22ddcabd291a
Viktor Verebelyi <vviktor2@gmail.com>
2019-08-08 09:58:25 +0000

    Fix build errors

--- stage3/01-tweaks/01-run.sh
+++ stage3/01-tweaks/01-run.sh
@@ -4,16 +4,16 @@ log "Getting udev rules"
 wget -P ${ROOTFS_DIR}/etc/udev/rules.d https://raw.githubusercontent.com/snowdream/51-android/master/51-android.rules
 
 log "Copy environment file"
-install -m 644 files/environment ${ROOTFS_DIR}/etc
-log "Copy headunit service file"
-install -m 644 files/headunit.service ${ROOTFS_DIR}/lib/systemd/system
-log "Copy pulseaudio config file"
-install -m 644 files/default.pa ${ROOTFS_DIR}/home/pi/.config/pulse
+install -v -d					                "${ROOTFS_DIR}/etc"
+install -m 644 files/environment                "${ROOTFS_DIR}/etc/"
 
-log "Run raspberry pi firmware update "
-on_chroot << EOF
-SKIP_WARNING=1 SKIP_BACKUP=1 rpi-update
-EOF
+log "Copy headunit service file"
+install -v -d					                "${ROOTFS_DIR}/lib/systemd/system"
+install -m 644 files/headunit.service           "${ROOTFS_DIR}/lib/systemd/system/"
+
+log "Copy pulseaudio config file"
+install -v -d					                "${ROOTFS_DIR}/home/pi/.config/pulse"
+install -m 644 files/default.pa                 "${ROOTFS_DIR}/home/pi/.config/pulse/"
 
 log "Enable services"
 on_chroot << EOF
