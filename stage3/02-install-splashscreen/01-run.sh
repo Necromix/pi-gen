0f49bb4bccaed4bd7beaab68f5e7210ef3018782
Viktor Verebelyi <vviktor2@gmail.com>
2019-09-17 19:58:55 +0000

    Fix plymouth theme install

--- stage3/02-install-splashscreen/01-run.sh
+++ stage3/02-install-splashscreen/01-run.sh
@@ -5,15 +5,8 @@ sed -i ' 1 s/.*/& splash plymouth.ignore-serial-consoles vt.global_cursor_defaul
 EOF
 
 log "Install plymouth-boot theme"
-install -m 644 files/box.png ${ROOTFS_DIR}/usr/share/plymouth/themes/headunit/
-install -m 644 files/bullet.png ${ROOTFS_DIR}/usr/share/plymouth/themes/headunit/
-install -m 644 files/dot.png ${ROOTFS_DIR}/usr/share/plymouth/themes/headunit/
-install -m 644 files/entry.png ${ROOTFS_DIR}/usr/share/plymouth/themes/headunit/
-install -m 644 files/headunit.plymouth ${ROOTFS_DIR}/usr/share/plymouth/themes/headunit/
-install -m 644 files/headunit.script ${ROOTFS_DIR}/usr/share/plymouth/themes/headunit/
-install -m 644 files/logo.png ${ROOTFS_DIR}/usr/share/plymouth/themes/headunit/
-install -m 644 files/progress_bar.png ${ROOTFS_DIR}/usr/share/plymouth/themes/headunit/
-install -m 644 files/progress_box.png ${ROOTFS_DIR}/usr/share/plymouth/themes/headunit/
+install -d "${ROOTFS_DIR}/usr/share/plymouth/themes/headunit/"
+install -m 644 files/* "${ROOTFS_DIR}/usr/share/plymouth/themes/headunit/"
 
 log "Set default plymouth theme and update initramfs"
 on_chroot << EOF
